cb9a1a0b29cfdb649506582eee52a5956ca3b04e
Rushi <rushitest4559@gmail.com>
2025-11-22 20:38:56 +0530

    aws amplify problem solved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,27 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-const queryClient = new QueryClient();
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Amplify } from 'aws-amplify'
+import '@aws-amplify/ui-react/styles.css'
 
-import {Amplify} from 'aws-amplify'
-import awsExports from './aws-exports.js'
-import '@aws-amplify/ui-react/styles.css';
+const queryClient = new QueryClient()
 
-const awsExports = import.meta.env.VITE_AWS_EXPORTS 
-  ? JSON.parse(import.meta.env.VITE_AWS_EXPORTS) 
-  : null;
+// THIS IS THE ONLY awsExports YOU NEED NOW
+const awsExports = import.meta.env.VITE_AWS_EXPORTS
+  ? JSON.parse(import.meta.env.VITE_AWS_EXPORTS)
+  : null
 
 if (!awsExports) {
-  console.error("Missing VITE_AWS_EXPORTS env var");
+  console.error('Missing VITE_AWS_EXPORTS env variable!')
 } else {
-  Amplify.configure(awsExports);
+  Amplify.configure(awsExports)
 }
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  // </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 )
